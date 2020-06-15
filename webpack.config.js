66ef6d8b82d5be9dee05cea41562ab22b2b70915
Silence <nt.studio.360@gmail.com>
2020-06-15 12:48:31 +0300

    установила webpack-dev-server, автообновление в браузере, запустила yarn dev, сверстала список с картинками в index.pug

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,12 @@ module.exports = {
         path: path.resolve(__dirname, './dist'),
         filename: 'main.js'
     },
+    devServer: {
+        host: 'localhost',
+        port: 3000,
+        stats: 'errors-only',
+        open: true
+    },
     module: {
         rules: [
             {
