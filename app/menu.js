4615761d4155a02f35e24b2cf6614d0d1bed3e33
Silence <nt.studio.360@gmail.com>
2020-06-16 14:54:44 +0300

    вынесла файлы блока меню в папку menu, создала миксин элемента меню

--- app/menu.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.js-menu-button').addEventListener('click', (e) => {
-    if(e.target.getAttribute('class') === 'menu-list-element-a')
-        alert('click' + e.target.innerText);
-}, false);
