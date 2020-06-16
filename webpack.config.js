10eeddb8ab768445cf47878a5078f9af5b737e40
Silence <nt.studio.360@gmail.com>
2020-06-16 19:33:04 +0300

    установила mini-css-extract-plugin, css-loader, style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+function htmlWebpackPlugin(name){
+    return new HtmlWebpackPlugin({
+        filename: `${name}.html`,
+        template: `./app/page/${name}.pug`
+    })
+}
 
 module.exports = {
     mode: 'development',
@@ -19,12 +27,21 @@ module.exports = {
             {
                 test: /\.pug$/,
                 use: ['pug-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader']
             }
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: './app/index.pug'
-        })
+        new MiniCssExtractPlugin({
+            filename: 'main.css'
+        }),
+        htmlWebpackPlugin('index'),
+        htmlWebpackPlugin('two'),
+        htmlWebpackPlugin('three'),
+        htmlWebpackPlugin('five'),
+        htmlWebpackPlugin('six')
     ]
 }
